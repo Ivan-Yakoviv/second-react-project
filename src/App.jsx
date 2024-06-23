420732ac767bfa16c9bbac61c4afdf2b95dcfb35
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-06-23 23:58:31 +0300

    create

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" }
+];
+
+const BookList = ({ books }) => {
+  return (
+		<ul>
+			{books.map((book) => {
+		return <li key={book.id}>{book.name}</li>;
+			})}
+		</ul>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+			<h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
+    </>
+  );
+};
+
+
+
+export default App
